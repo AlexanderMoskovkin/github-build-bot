bedb7815bf0b7c27e7cab596a87d0bd57b328e29
Alex Mos <alexander.moskovkin@devexpress.com>
2016-08-12 18:17:53 +0300

    Get rid of old tests

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,6 @@ var babel  = require('gulp-babel');
 var del    = require('del');
 var eslint = require('gulp-eslint');
 var gulp   = require('gulp');
-var mocha  = require('gulp-mocha');
 
 gulp.task('lint', function () {
     return gulp
@@ -21,14 +20,4 @@ gulp.task('build', ['clean', 'lint'], function () {
         .src('src/**/*.js')
         .pipe(babel())
         .pipe(gulp.dest('./lib'));
-});
-
-gulp.task('test', ['build'], function () {
-    return gulp
-        .src('test/*-test.js')
-        .pipe(mocha({
-            ui:       'bdd',
-            reporter: 'spec',
-            timeout:  typeof v8debug === 'undefined' ? 2000 : Infinity // NOTE: disable timeouts in debug
-        }));
 });
