e21df95979da8a2d992625871bc382f747828028
Andrey Belym <belym.a.2105@gmail.com>
2016-09-09 16:36:59 +0300

    Delete appveyor.yml in [docs] PRs

--- src/messages-handler.js
+++ src/messages-handler.js
@@ -84,10 +84,16 @@ export default class MessagesHandler {
             return this.github.createBranch(repo, prSha, branchName);
 
         var temporaryBranchName = MessagesHandler._getTemporaryBranchName(branchName);
+        var commitMessage       = '';
 
         this.github.createBranch(repo, prSha, temporaryBranchName)
             .then(() => this.github.getCommitMessage(repo, owner, prSha))
-            .then(commitMessage => {
+            .then(message => {
+                commitMessage = message;
+
+                return this.github.deleteFile(repo, 'appveyor.yml', temporaryBranchName, commitMessage);
+            })
+            .then(() => {
                 return this.github.replaceFile(repo, '.travis.yml', `.travis-${travisConf}.yml`,
                     temporaryBranchName, commitMessage);
             })
@@ -107,10 +113,16 @@ export default class MessagesHandler {
             return this.github.syncBranchWithCommit(repo, branchName, prSha);
 
         var temporaryBranchName = MessagesHandler._getTemporaryBranchName(branchName);
+        var commitMessage       = '';
 
         this.github.syncBranchWithCommit(repo, temporaryBranchName, prSha)
             .then(() => this.github.getCommitMessage(repo, owner, prSha))
-            .then(commitMessage => {
+            .then(message => {
+                commitMessage = message;
+
+                return this.github.deleteFile(repo, 'appveyor.yml', temporaryBranchName, commitMessage);
+            })
+            .then(() => {
                 return this.github.replaceFile(repo, '.travis.yml', `.travis-${travisConf}.yml`,
                     temporaryBranchName, commitMessage);
             })
